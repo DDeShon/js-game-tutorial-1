8c94e6ac15d582a18a81821671d8fb3a9c21ca67
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-06-02 22:37:48 -0500

    working on animation

--- script.js
+++ script.js
@@ -10,13 +10,38 @@ const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
-const staggerFrames = 6;
+const staggerFrames = 4;
+const spriteAnimations = [];
+const animationStates = [
+  {
+    name: "idle",
+    frames: "7",
+  },
+  {
+    name: "jump",
+    frames: "7",
+  },
+];
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
+console.log(animationStates);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  let position = Math.floor(gameFrame / staggerFrames) % 6;
+  frameX = spriteWidth * position;
   ctx.drawImage(
     playerImage,
-    frameX * spriteWidth,
+    frameX,
     frameY * spriteHeight,
     spriteWidth,
     spriteHeight,
