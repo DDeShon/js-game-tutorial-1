25775720b5c03b22186e9a4add95db822a21930a
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-06-03 22:31:02 -0500

    working on animation

--- script.js
+++ script.js
@@ -13,11 +13,31 @@ const spriteAnimations = [];
 const animationStates = [
   {
     name: "idle",
-    frames: "7",
+    frames: 7,
   },
   {
     name: "jump",
-    frames: "7",
+    frames: 7,
+  },
+  {
+    name: "fall",
+    frames: 9,
+  },
+  {
+    name: "run",
+    frames: 9,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "roll",
+    frames: 7,
   },
 ];
 animationStates.forEach((state, index) => {
